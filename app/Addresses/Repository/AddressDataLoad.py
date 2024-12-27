096ade0ac3c7b950674c2734b07a2a43f4c51d62
Bart Gelders <gelders.bart@odyssey-consulting.be>
2024-12-27 15:06:42 +0100

    feat: added db index on addresses table for PC
    Refs: GIM-001

--- app/Addresses/Repository/AddressDataLoad.py
+++ app/Addresses/Repository/AddressDataLoad.py
@@ -2,10 +2,12 @@ from app.Addresses.Repository.AddressRepo import get_write_db_connection
 
 file_path = 'Addresses/Repository/input_data.csv'
 
+
 def load_address_data():
     with get_write_db_connection() as connection:
         drop_table(connection)
         load_data_from_csv(connection)
+        create_index(connection)
         print("Address data loaded")
 
 
@@ -20,3 +22,9 @@ def load_data_from_csv(connection):
     CREATE TABLE addresses AS 
     SELECT * FROM read_csv_auto('{file_path}')
     """)
+
+def create_index(connection):
+    connection.execute("""
+       CREATE INDEX addresses_PC_index ON addresses (PC);
+       """)
+
